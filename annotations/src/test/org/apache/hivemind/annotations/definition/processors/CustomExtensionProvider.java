08ebe995d6bd64cf99ccd112dedd4b159a6ad318
ahuegen <ahuegen@localhost>
2007-03-29 20:16:05 +0000

    Unit test fixed (temporarily)

---
+++ annotations/src/test/org/apache/hivemind/annotations/definition/processors/CustomExtensionProvider.java
@@ -0,0 +1,14 @@
+package org.apache.hivemind.annotations.definition.processors;
+
+import org.apache.hivemind.annotations.AnnotationsExtensionProvider;
+import org.apache.hivemind.annotations.internal.AnnotationProcessorRegistry;
+
+public class CustomExtensionProvider implements AnnotationsExtensionProvider
+{
+
+    public void registerAnnotationProcessors(AnnotationProcessorRegistry registry)
+    {
+//        registry.registerProcessor(annotationClass, processorClass)
+    }
+
+}
